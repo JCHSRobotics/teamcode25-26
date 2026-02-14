b67889182104b5e40afbc77b6b17d5ecf70ff303
TechLabE7 <jchsroboticsengineering@gmail.com>
2026-02-14 03:32:18 -0800

    Setup motors with Ids in constats file

--- TeamCode/src/main/java/Subsystems/drive/driveConstants.java
+++ TeamCode/src/main/java/Subsystems/drive/driveConstants.java
@@ -1,3 +1,8 @@
 public class driveConstants {
 
+    public final static string ID1 = "FrontRightDrive";
+    public final static string ID2 = "FrontLeftDrive";
+    public final static string ID3 = "BackRightDrive";
+    public final static string ID4 = "BackLeftDrive";
+
 }
